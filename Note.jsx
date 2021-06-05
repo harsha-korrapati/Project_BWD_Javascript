c398d119e3b156b4d707c0a2e1e3817ae2165cb8
harsha-korrapati <84908165+harsha-korrapati@users.noreply.github.com>
2021-06-05 17:12:27 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Bootcamp of Javascript and React.js</h1>
+      <p>
+        This bootcamp is of Web development with Javascript and React.js for 7 days .
+        This was really an amazing bootcamp from Shaurya sir and we really learned a lot from this bootcamp .
+        We are interested in participating in upcoming bootcamps by SHAPE AI .
+        </p>
+    </div>
+  );
+}
+
+export default Note;
